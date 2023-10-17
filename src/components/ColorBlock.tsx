0472cdb669a0a93a407da9d0df0bc4d9ab13f39f
Minna Svartback <minna.svartback@pm.me>
2023-10-17 22:36:58 +0300

    Remove unneccessary lines

--- src/components/ColorBlock.tsx
+++ src/components/ColorBlock.tsx
@@ -3,8 +3,6 @@ import { backendStub } from '../utils/backendStub';
 export function ColorBlock(props) {
 	return (
 		<div class='color-block' style={props.style}>
-			<h2>{props.title}</h2>
-			<p>{props.description}</p>
 		</div>
 	);
 }
