9674c9bd78c2ac63749ed0e733cc30a33475c2f1
Minna Svartback <minna.svartback@pm.me>
2023-10-18 12:36:10 +0300

    set backend url from environment variable

--- src/utils/backendRequests.ts
+++ src/utils/backendRequests.ts
@@ -11,7 +11,7 @@ export const getColor = async () => {
 
 export const checkColor = async (guess: string, answer: string) => {
     try {
-        const response = await fetch('http://127.0.0.1:5000/checkcolor', {
+        const response = await fetch('${BACKEND_URL}/checkcolor', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
